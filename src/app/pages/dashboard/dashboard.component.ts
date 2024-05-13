5d9c62d9347a5075def5eb39688a00bfeb29bf08
phuongtrang20 <145195788+phuongtrang20@users.noreply.github.com>
2024-05-13 13:34:32 +0700

    04 - danh muc, dia diem, dashboard

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-dashboard',
+  styleUrls: ['./dashboard.component.scss'],
+  templateUrl: './dashboard.component.html',
+})
+export class DashboardComponent {}
